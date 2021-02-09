47d8a7dc9c84caee918773fcf55c95005c038dc8
NicholasBenenati <78377689+NicholasBenenati@users.noreply.github.com>
2021-02-08 21:05:12 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_7v51dcNUl', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '24a19t88tlnern9j8u6rlajbhd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-ease-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
